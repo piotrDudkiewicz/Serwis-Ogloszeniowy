ed39ebdedc77aae108fbebc0615f579322076d1c
piter4d <pidudkiewicz632@gmail.com>
2020-05-19 15:50:09 +0200

    Lab11 Full

---
+++ ogloszenia/src/main/java/wizut/tpsi/ogloszenia/HomeController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wizut.tpsi.ogloszenia;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import wizut.tpsi.ogloszenia.jpa.BodyStyle;
+import wizut.tpsi.ogloszenia.jpa.CarManufacturer;
+import wizut.tpsi.ogloszenia.jpa.CarModel;
+import wizut.tpsi.ogloszenia.jpa.FuelType;
+import wizut.tpsi.ogloszenia.jpa.Offer;
+import wizut.tpsi.ogloszenia.services.OffersService;
+
+/**
+ *
+ * @author Pioterk
+ */
+@Controller
+public class HomeController {
+
+    @Autowired
+    private OffersService offersService;
+
+    @RequestMapping("/")
+    public String home(Model model) {
+        List<CarManufacturer> carManufacturers = offersService.getCarManufacturers();
+        List<CarModel> carModels = offersService.getCarModels();
+
+        List<Offer> offers = offersService.getOffers();
+
+        model.addAttribute("carManufacturers", carManufacturers);
+        model.addAttribute("carModels", carModels);
+        model.addAttribute("offers", offers);
+
+        return "offersList";
+    }
+
+    @GetMapping("/offer/{id}")
+    public String offerDetails(Model model, @PathVariable("id") Integer id) {
+        Offer offer = offersService.getOffer(id);
+        model.addAttribute("offer", offer);
+        return "offerDetails";
+    }
+}
